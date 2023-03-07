555de2dab13bd7caf8f7a74090b063272b3044f9
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-07 14:00:59 +0000

    In the making

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+  end
+
+  def edit
+  end
+end
+
+ private
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :name)
+  end
